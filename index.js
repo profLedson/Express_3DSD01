fb14844498ff804c9b83612a66a259f78c0a10d1
LEDSON J VANINI <ledson.vanini@escola.pr.gov.br>
2024-10-04 11:38:58 +0000

    Criou rota 404 e iniciou o menu

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const app = express();
 
 const PORT = 3333; // 65K
+// CRIAÇÃO DAS ROTAS
+//app.use(express.static(__dirname + "public"));
 
 app.get("/", function (req, res) {
-  res.send("Bla bla bla!");
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+// CRIAR ROTA 404 (MIDDLEWARE)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
-  console.log("running... " + PORT);
+  console.log("running...! " + PORT);
 });
