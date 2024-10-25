3c01b822d2e317d8c1fde0feec6e0560fc15cb1c
LEDSON J VANINI <ledson.vanini@escola.pr.gov.br>
2024-10-25 11:49:06 +0000

    Rotas, ajustes do css + criação de pagins

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get("/", function (req, res) {
 app.get("/sobre", function (req, res) {
   res.sendFile(__dirname + "/src/views/about.html");
 });
+app.get("/contato", function (req, res) {
+  res.sendFile(__dirname + "/src/views/contato.html");
+});
+
+app.get("/logar", function (req, res) {
+  res.sendFile(__dirname + "/src/views/logar.html");
+});
+
 // CRIAR ROTA 404 (MIDDLEWARE)
 app.use(function (req, res) {
   res.status(404).sendFile(__dirname + "/src/views/404.html");
